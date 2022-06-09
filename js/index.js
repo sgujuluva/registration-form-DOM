931cca8e3702ceb0b5c8abc611b5eacbf5a5b953
sgujuluva <sanghee04@gmail.com>
2022-06-09 12:25:48 +0200

    pageone birthdate

--- js/index.js
+++ js/index.js
@@ -26,3 +26,35 @@ function previous(element) {
       break;
   }
 }
+
+
+let personalDiv = document.querySelector("#personalDiv");
+
+let addressDiv = document.querySelector("#addressDiv");
+//next button
+let nextBtn = document.querySelector(".registerbtn");
+// input
+let firstName = document.querySelector("#personalDiv #fName");
+let lastName = document.querySelector("#personalDiv #lName");
+let birthDate = document.querySelector("#personalDiv #bDate");
+let reqNationality = [];
+let nationality = document.querySelectorAll("#personalDiv select option");
+/* nationality.forEach(i => console.log(i)) */
+/* for(let i = 0; i < nationality.length; i++){
+  reqNationality.push(nationality[i]) 
+}
+console.log(reqNationality) */
+/* let allInputs = document.querySelectorAll("#personalDiv input");
+console.log(allInputs)
+let input = allInputs.forEach(i => i);
+console.log(input) */
+//check validatity
+function checkValidity(){
+
+  if(!firstName.value == "" && !lastName.value == "" && !birthDate.value == 0 && nationality.forEach(i => !i.value == i[1])  ){
+    personalDiv.style.display = "none"
+    addressDiv.style.display = "block"
+  } 
+ 
+}
+nextBtn.addEventListener("click",checkValidity );
