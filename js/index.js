45b3a0c206f0153efbcaea5fb078cccd9628731b
sgujuluva <sanghee04@gmail.com>
2022-06-09 15:47:39 +0200

    3rd page form in js done

--- js/index.js
+++ js/index.js
@@ -27,28 +27,67 @@ function previous(element) {
   }
 }
 
-
+//page1
 let personalDiv = document.querySelector("#personalDiv");
 
 let addressDiv = document.querySelector("#addressDiv");
-//next button
-let nextBtn = document.querySelector(".registerbtn");
-// input
+
+let profileDiv = document.querySelector("#profileDiv");
+
+let resultDiv = document.querySelector("#resultDiv");
+
+//next button for personalDiv
+let nextBtn = document.querySelector("#personalDiv .registerbtn");
+//next button for addressDiv
+let nextBtn1 = document.querySelector("#addressBtn");
+//next button for personalinfo
+let nextBtn2 = document.querySelector("#profileBtn");
+
+// input of page1
 let firstName = document.querySelector("#personalDiv #fName");
 
 let lastName = document.querySelector("#personalDiv #lName");
 let birthDate = document.querySelector("#personalDiv #bDate");
 let nationality = document.querySelector("#personalDiv select ");
 
+//input of 2nd page
+let address = document.querySelector("#address-input");
+console.log(address)
+let houseNumber = document.querySelector("#house-number");
+console.log(houseNumber)
+let zipCode= document.querySelector("#zip-code");
+console.log(zipCode)
+
+//input 3rd page
+let userName = document.querySelector("#user-name");
+let password = document.querySelector("#password");
+let repeatPassword = document.querySelector("#repeat-password");
+let photo = document.querySelector("#photo");
+
+
+
+ personalDiv.style.display = "none"
+    addressDiv.style.display = "none" 
+    profileDiv.style.display = "block" 
 
 //check validatity
-function page1Form(){
+function form(){
 
   if(firstName.checkValidity() && lastName.checkValidity() &&birthDate.checkValidity() && nationality.checkValidity())
   {
     personalDiv.style.display = "none"
     addressDiv.style.display = "block"
-  } 
+  } else if(address.checkValidity() && houseNumber.checkValidity() && zipCode.checkValidity())
+  {
+        addressDiv.style.display = "none"
+    profileDiv.style.display = "block"
+  }else if(userName.checkValidity()&& password.checkValidity() && repeatPassword.checkValidity() &&photo.checkValidity() &&(password.value === repeatPassword.value)){
+    profileDiv.style.display = "none"
+    resultDiv.style.display = "block"
+  }
  
 }
-nextBtn.addEventListener("click",page1Form );
+nextBtn.addEventListener("click",form );
+nextBtn1.addEventListener("click",form );
+nextBtn2.addEventListener("click",form );
+
