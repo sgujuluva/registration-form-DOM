ff8d35e4e0842fd92d375c34e6cb1944edf19d8e
sgujuluva <sanghee04@gmail.com>
2022-06-09 19:59:02 +0200

    form done except photo

--- js/index.js
+++ js/index.js
@@ -1,48 +1,3 @@
-function previous(element) {
-  let fatherDiv = element.parentNode.id;
-  switch (fatherDiv) {
-    case 'addressDiv':
-      element.parentNode.style.display = 'none';
-      document.getElementById('personalDiv').style.display = 'block';
-      document.getElementById('profileDiv').style.display = 'none';
-      document.getElementById('resultDiv').style.display = 'none';
-      break;
-    case 'profileDiv':
-      element.parentNode.style.display = 'none';
-      document.getElementById('personalDiv').style.display = 'none';
-      document.getElementById('addressDiv').style.display = 'block';
-      document.getElementById('resultDiv').style.display = 'none';
-
-      break;
-    case 'resultDiv':
-      element.parentNode.style.display = 'none';
-      document.getElementById('personalDiv').style.display = 'none';
-      document.getElementById('addressDiv').style.display = 'none';
-      document.getElementById('profileDiv').style.display = 'block';
-
-      break;
-
-    default:
-      break;
-  }
-}
-
-//page1
-let personalDiv = document.querySelector("#personalDiv");
-
-let addressDiv = document.querySelector("#addressDiv");
-
-let profileDiv = document.querySelector("#profileDiv");
-
-let resultDiv = document.querySelector("#resultDiv");
-
-//next button for personalDiv
-let nextBtn = document.querySelector("#personalDiv .registerbtn");
-//next button for addressDiv
-let nextBtn1 = document.querySelector("#addressBtn");
-//next button for personalinfo
-let nextBtn2 = document.querySelector("#profileBtn");
-
 // input of page1
 let firstName = document.querySelector("#personalDiv #fName");
 
@@ -52,11 +7,10 @@ let nationality = document.querySelector("#personalDiv select ");
 
 //input of 2nd page
 let address = document.querySelector("#address-input");
-console.log(address)
+
 let houseNumber = document.querySelector("#house-number");
-console.log(houseNumber)
-let zipCode= document.querySelector("#zip-code");
-console.log(zipCode)
+
+let zipCode = document.querySelector("#zip-code");
 
 //input 3rd page
 let userName = document.querySelector("#user-name");
@@ -64,14 +18,106 @@ let password = document.querySelector("#password");
 let repeatPassword = document.querySelector("#repeat-password");
 let photo = document.querySelector("#photo");
 
+//input for 4th page
+let name = document.querySelector("#nameLabel");
+let birthDatelabel = document.querySelector("#birthDateLabel");
+let nationalitylabel = document.querySelector("#nationalityLabel");
+let addresslabel = document.querySelector("#addressLabel");
+let usernameLabel = document.querySelector("#usernameLabel");
+let passwordLabel = document.querySelector("#passwordLabel");
+let photoLabel = document.querySelector("#photoLabel");
 
+//previous button
+function previous(element) {
+  let fatherDiv = element.parentNode.id;
+  switch (fatherDiv) {
+    case "addressDiv":
+      element.parentNode.style.display = "none";
+      document.getElementById("personalDiv").style.display = "block";
+      document.getElementById("profileDiv").style.display = "none";
+      document.getElementById("resultDiv").style.display = "none";
+      break;
+    case "profileDiv":
+      element.parentNode.style.display = "none";
+      document.getElementById("personalDiv").style.display = "none";
+      document.getElementById("addressDiv").style.display = "block";
+      document.getElementById("resultDiv").style.display = "none";
 
- personalDiv.style.display = "none"
-    addressDiv.style.display = "none" 
-    profileDiv.style.display = "block" 
+      break;
+    case "resultDiv":
+      element.parentNode.style.display = "none";
+      document.getElementById("personalDiv").style.display = "none";
+      document.getElementById("addressDiv").style.display = "none";
+      document.getElementById("profileDiv").style.display = "block";
+
+      break;
+
+    default:
+      break;
+  }
+}
+
+//next button
+function nextDiv(element) {
+  let fatherDiv = element.parentNode.id;
+  switch (fatherDiv) {
+    case "personalDiv":
+      if (
+        firstName.checkValidity() &&
+        lastName.checkValidity() &&
+        birthDate.checkValidity() &&
+        nationality.checkValidity()
+      ) {
+        element.parentNode.style.display = "none";
+        document.getElementById("personalDiv").style.display = "none";
+        document.getElementById("addressDiv").style.display = "block";
+        document.getElementById("resultDiv").style.display = "none";
+        name.innerHTML = firstName.value + lastName.value;
+        birthDatelabel.innerHTML = birthDate.value;
+        nationalitylabel.innerHTML = nationality.value;
+      }
+
+      break;
+    case "addressDiv":
+      if (
+        address.checkValidity() &&
+        houseNumber.checkValidity() &&
+        zipCode.checkValidity()
+      ) {
+        element.parentNode.style.display = "none";
+        document.getElementById("personalDiv").style.display = "none";
+        document.getElementById("profileDiv").style.display = "block";
+        document.getElementById("resultDiv").style.display = "none";
+        addresslabel.innerHTML = address.value +  houseNumber.value  +  zipCode.value ;
+        }
+
+      break;
+    case "profileDiv":
+      if (
+        userName.checkValidity() &&
+        password.checkValidity() &&
+        repeatPassword.checkValidity() &&
+        photo.checkValidity() &&
+        password.value === repeatPassword.value
+      ) {
+        element.parentNode.style.display = "none";
+        document.getElementById("personalDiv").style.display = "none";
+        document.getElementById("addressDiv").style.display = "none";
+        document.getElementById("resultDiv").style.display = "block";
+        usernameLabel.innerHTML = userName.value;
+        passwordLabel.innerHTML = password.value;
+        photoLabel.append(photo.value);
+      }
+
+      break;
+
+    default:
+      break;
+  }
+}
 
 //check validatity
-function form(){
+/* function form(){
 
   if(firstName.checkValidity() && lastName.checkValidity() &&birthDate.checkValidity() && nationality.checkValidity())
   {
@@ -85,9 +131,16 @@ function form(){
     profileDiv.style.display = "none"
     resultDiv.style.display = "block"
   }
- 
+  name.innerHTML = firstName.value+lastName.value;
+   birthDatelabel = 02/03/1972
+  nationalitylabel.innerHTML ="indi"
+  addresslabel.innerHTML="jsdjgh"
+  usernameLabel.innerHTML="hdslgh"
+  passwordLabel.innerHTML=""
+  photoLabel.innerHTML 
 }
 nextBtn.addEventListener("click",form );
 nextBtn1.addEventListener("click",form );
 nextBtn2.addEventListener("click",form );
 
+ */
